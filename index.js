637bf0006aa633314be932832d6ad08dcf23bad0
Watcharapol Watcharawisetkul <groupw@jitta.com>
2017-05-03 17:06:06 +0700

    Add common eslint config

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb-base",
+  "plugins": [
+    "flowtype"
+  ],
+  "rules": {
+    "semi": 0,
+    "no-underscore-dangle": 0,
+    "comma-dangle": 0,
+    "camelcase": 0,
+    "no-undef": 0,
+    "no-param-reassign": ["error", { "props": false }],
+    "no-use-before-define": 0,
+  },
+}
