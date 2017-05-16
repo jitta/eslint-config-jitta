d636f8bcb2e81fb6e854cac182192a8bf7295891
Ham Chao <pallop.chao@gmail.com>
2017-05-16 12:11:52 +0700

    Exclude `no-undef` rule

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ module.exports = {
     "no-underscore-dangle": 0,
     "comma-dangle": 0,
     "camelcase": 0,
-    "no-undef": 0,
     "no-param-reassign": ["error", { "props": false }],
     "no-use-before-define": 0,
   },
