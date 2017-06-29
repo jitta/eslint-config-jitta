2b706b934b4defa9b338dfcc542d5b629bff5182
Watcharapol Watcharawisetkul <groupw@jitta.com>
2017-06-29 08:45:57 +0700

    Disable comma-dangle for functions

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ module.exports = {
     "semi": ["warn", "never"],
     "no-underscore-dangle": "off",
     "no-param-reassign": ["error", { "props": false }],
-    "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }]
+    "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "never",
+    }]
   }
 }
