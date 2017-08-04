b7e113c2c537067f6d62a52d869337140253b3cf
Nuttapon Pattanavijit <nut@jitta.com>
2017-08-04 13:11:05 +0700

    disallows trailing commas

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     "semi": ["warn", "never"],
     "no-underscore-dangle": "off",
     "no-param-reassign": ["error", { "props": false }],
-    "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }]
+    "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }],
+    "comma-dangle": ["error", "never"]
   }
 }
