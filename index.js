b88ae9a93a7ad2efff93f0c184fc3e3d026fd63c
Sira Sujjinanont <hunt@jitta.com>
2017-08-16 02:04:28 +0700

    Add ignoreComments to max-len

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ module.exports = {
     "no-underscore-dangle": "off",
     "no-param-reassign": ["error", { "props": false }],
     "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }],
-    "comma-dangle": ["error", "never"]
+    "comma-dangle": ["error", "never"],
+    "max-len": ["error", 100, 2, {
+      "ignoreUrls": true,
+      "ignoreComments": true,
+      "ignoreRegExpLiterals": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+    }]
   }
 }
