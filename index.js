2c6c9dcda412dac6e4df235a9894ebc6f65112f5
Ham Chao <pallop.chao@gmail.com>
2018-03-19 14:10:01 +0700

    Upgrade dependencies and add Jest (#14)
    * Upgrade dependencies and add Jest
    
    * 0.2.0

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ module.exports = {
   "parser": "babel-eslint",
   "extends": "airbnb",
   "plugins": [
-    "flowtype"
+    "flowtype",
+    "jest"
   ],
+  "env": {
+    "jest/globals": true
+  }
   "rules": {
     "semi": ["warn", "never"],
     "no-underscore-dangle": "off",
     "no-param-reassign": ["error", { "props": false }],
     "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }],
-    "comma-dangle": ["error", "never"]
+    "comma-dangle": ["error", "never"],
+    "prefer-destructuring": "off",
+    "function-paren-newline": "off"
   }
 }
